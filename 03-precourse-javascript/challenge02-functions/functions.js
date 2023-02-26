8291cdec0e62ada10878daae3b0c46299a34cfbc
Kenny Jewett <kennethrjewett@gmail.com>
2023-02-26 14:23:11 -0600

    Did finctions challenge. Instructor example for final challenge did not work. Instructor also never showed his example working.

---
+++ 03-precourse-javascript/challenge02-functions/functions.js
@@ -0,0 +1,27 @@
+// **Challenge**
+
+// 1. Write a function to find the perimeter of a rectangle.
+// const calculatePerimeter = (width, height) => {
+//     return width + width + height + height;
+// }
+
+
+// // 2. Write a function to find the area of a circle. When given a radius, print the 
+// // area to the console.
+
+// const calculateArea = (radius) => {
+//     const area = Math.PI * radius * radius;
+//     return area;
+// }
+
+// calculateArea(2);
+
+
+// 3. Now do the same to find the circumference of a circle...
+
+const calculatePerimeter = (radius) => {
+    return 2 * Math.PI * radius;    
+    
+}
+
+calculatePerimeter(1);
