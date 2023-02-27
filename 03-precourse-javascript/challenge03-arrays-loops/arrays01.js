c466ced2bde8d3e8182934abaa62a84547e30e06
Kenny Jewett <kennethrjewett@gmail.com>
2023-02-27 16:15:34 -0600

    Added code along loops.

---
+++ 03-precourse-javascript/challenge03-arrays-loops/arrays01.js
@@ -0,0 +1,21 @@
+// 1. Create a coaches array
+
+const coaches = ["Andy", "Sam", "Bex"];
+
+// 2. Create an array of pets
+
+const pets = ["Fido" , "Rusty", "Jack"];
+
+// 3. Create a sample for loop
+
+// for (let index = 0; index < 5; index++) {
+//     console.log(`The index is ${index}`);
+// }
+
+for (let i = 0; i < coaches.length; i++) {
+    console.log(coaches[i]);
+}
+
+for (let i = 0; i < pets.length; i++) {
+    console.log(pets[i]);
+}
