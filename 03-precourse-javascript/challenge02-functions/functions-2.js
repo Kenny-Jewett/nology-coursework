f75708f1713c9b070547758e12b711672c859eb7
Kenny Jewett <kennethrjewett@gmail.com>
2023-02-27 14:07:21 -0600

    Finished Snickers challenge.

--- 03-precourse-javascript/challenge02-functions/functions-2.js
+++ 03-precourse-javascript/challenge02-functions/functions-2.js
@@ -4,16 +4,48 @@
 // "I am currently a X living in Y, and married to Z with N kids."
 // Call that function 3 times with 3 different values for the arguments.
 
-function getDescription (jobTitle, location, partnerName, numberOfChildren) {
+// function getDescription (jobTitle, location, partnerName, numberOfChildren) {
 
-    console.log(`I am currently a ${jobTitle} living in ${location}, and married 
-    to ${partnerName} with ${numberOfChildren} kids.`);
+//     console.log(`I am currently a ${jobTitle} living in ${location}, and married 
+//     to ${partnerName} with ${numberOfChildren} kids.`);
 
 
-}
+// }
 
-getDescription(`Accountant`, `Texas`, `Sam`, `four`);
-getDescription(`Teacher`, `Florida`, `Tim`, `1`);
-getDescription(`Firefighter`, `Maine`, `John`, `two`);
-getDescription(`Police officer`, `Arkansas`, `Boris`, `no`);
+// getDescription(`Accountant`, `Texas`, `Sam`, `four`);
+// getDescription(`Teacher`, `Florida`, `Tim`, `1`);
+// getDescription(`Firefighter`, `Maine`, `John`, `two`);
+// getDescription(`Police officer`, `Arkansas`, `Boris`, `no`);
+
+
+// **The Lifetime Supply of Snickers**
+
+// Ever wonder how much a lifetime supply of Snickers would actually be? Wonder no more!
+
+// Write a function named caculateSupply that:
+// takes 2 arguments: age, amount per day.
+// calculates the amount consumed for rest of your life (based on a constant max age).
+// outputs the result to the console like so: "You will need X to last you until the ripe 
+// old age of Y"
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and
+// round the result to a round number.
+
+// function calculateSupply (age, amountPerDay) {
+//     const maxAge = 100;
+//     const calculateYears = maxAge - age;
+//     const totalAmount = (calculateYears * amountPerDay) * 365;
+//     console.log(`You will need ${totalAmount} Snickers to last you until the ripe old age of ${maxAge}`);
+
+// }
+
+// calculateSupply(50, 2);
+// calculateSupply(30, 3);
+// calculateSupply(80, 1);
+// calculateSupply(20, 5);
+
+
+
+
+
 
